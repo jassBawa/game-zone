9e9f33a80f075871880ef4d2af6e0c5b271c9008
jasspreetbawa <jaspreetbawa@ryaz.io>
2025-01-09 23:02:24 +0530

    fix: reactga

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import ReactGA from "react-ga4";
 
 const client = new QueryClient();
+
+ReactGA.initialize('G-KCVZ9QZK65')
+
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname
+})
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
